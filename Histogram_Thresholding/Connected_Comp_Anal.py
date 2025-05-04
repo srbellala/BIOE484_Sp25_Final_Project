277037e6e4f72d9569a25191e99553ac77c53745
Andres Orr <aorr3@illinois.edu>
2025-05-04 15:42:51 -0500

    changed connected component code and visulized in Demo file

--- Histogram_Thresholding/Connected_Comp_Anal.py
+++ Histogram_Thresholding/Connected_Comp_Anal.py
@@ -16,16 +16,11 @@ def Conn_Comp_Anal(image):
         centroids: center coordinates; (x,y)-coordinates of each connected component
     """
 
-    #construct argument parser
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required=True, help="path to input image")
-    ap.add_argument("-c", "--connectivity", type=int, default = 4, help = "connectivity for connected component analysis")
-    args=vars(ap.parse_args())
 
     #Perform the Connected components Analysis
-    thresh_labeled = cv2.connectedComponentsWithStats(image, args["Connectivity"], cv2.CV_32S)
+    thresh_labeled = cv2.connectedComponents(image)
 
-    #Retrieve the 4-tuple from the connected component analysis
-    (numlabels, labels, stats, centroids) = thresh_labeled
+    #Retrieve the tuple from the connected component analysis
+    (numlabels, labels) = thresh_labeled
 
-    return numlabels, labels, stats, centroids
+    return numlabels, labels
